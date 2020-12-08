9daaed00781f94f79021d2f34f929a79e8ebbec2
Mike Nanto <mike@thecowboymechanic.com>
2020-12-08 04:25:15 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,9 +1,6 @@
 # code the #greeting method here!
 puts "Hi! I'm HAL, what's your name?"
-
-def greeting(name)
-  name = gets.strip
+name = gets.strip
+def greeting
+  puts "Hello #{name}. It's nice to meet you."
 end
-
-greeting
-puts "Hello #{name}. It's nice to meet you."
